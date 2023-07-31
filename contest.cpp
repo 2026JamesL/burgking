97eb0866f822f9ce07f2426f33467997eb3b0167
fortybyte <66460857+fortybyte@users.noreply.github.com>
2023-07-30 18:27:16 -0700

    Create contest.cpp
    codeforces contest template

---
+++ contest.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class def{
+  #define ll long long
+  #define lb long double
+  //template u can change ig
+}
+void solve() {
+
+}
+
+signed main(){
+    int t;
+    cin >> t;
+    while(t--)solve();
+    return 0;
+}
